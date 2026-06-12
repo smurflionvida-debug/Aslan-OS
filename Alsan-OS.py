ddbf31c025dae55a6ce657ea62887f996ec0edb9
Lionsmurf <smurflionvida@gmail.com>
2026-06-12 10:31:25 -0500

    Implement date input functionality in Alsan-OS
    Added a simple date input feature to Alsan-OS.
    
    Signed-off-by: Lionsmurf <smurflionvida@gmail.com>

---
+++ Alsan-OS.py
@@ -0,0 +1,33 @@
+import datetime
+
+x = datetime.datetime(1900, 1, 1)
+
+print(x)
+print ("Lion OS 0.0.0")
+"Hostman"
+print("Enter the day:")
+dateday = input()
+print(f"Day {dateday}")
+
+print("Enter the Month:")
+datemonth = input()
+print(f"Month {datemonth}")
+
+print("Enter the Year:")
+Year = input()
+print(f"Year {Year}")
+print(f"Date {Year}{"/"}{datemonth}{"/"}{dateday}")
+
+if input ("date"):
+    print("Enter the day:")
+dateday = input()
+print(f"Day {dateday}")
+
+print("Enter the Month:")
+datemonth = input()
+print(f"Month {datemonth}")
+
+print("Enter the Year:")
+Year = input()
+print(f"Year {Year}")
+print(f"Date {Year}{"/"}{datemonth}{"/"}{dateday}")
