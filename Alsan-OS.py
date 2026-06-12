fa8e6b68ecccd00cbcf717003f7f689e0258eaf7
Lionsmurf <smurflionvida@gmail.com>
2026-06-12 11:53:47 -0500

    Update version number and simplify date input
    Updated version number and removed redundant date input prompts. and make it that it works
    
    Signed-off-by: Lionsmurf <smurflionvida@gmail.com>

--- Alsan-OS.py
+++ Alsan-OS.py
@@ -3,23 +3,9 @@ import datetime
 x = datetime.datetime(1900, 1, 1)
 
 print(x)
-print ("Aslan OS 0.0.0")
+print ("Aslan OS 0.0.0.1")
 "Hostman"
 print("Enter the day:")
-dateday = input()
-print(f"Day {dateday}")
-
-print("Enter the Month:")
-datemonth = input()
-print(f"Month {datemonth}")
-
-print("Enter the Year:")
-Year = input()
-print(f"Year {Year}")
-print(f"Date {Year}{"/"}{datemonth}{"/"}{dateday}")
-
-if input ("date"):
-    print("Enter the day:")
 dateday = input()
 print(f"Day {dateday}")
 
